a4a16aa2004f420377c0f6a57640178110522f7f
David LeGare <excaliburHisSheath@gmail.com>
2014-07-19 17:57:07 -0500

    Cleanup I forgot to clean up some of my testing/debugging stuff from my last commit.

--- JetSetRockets/Assets/Scripts/PlayerGUI.cs
+++ JetSetRockets/Assets/Scripts/PlayerGUI.cs
@@ -7,10 +7,6 @@ public class PlayerGUI : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.Box( new Rect( cam.rect.x * Screen.width, (1 - cam.rect.height - cam.rect.y) * Screen.height, 10, 10 ), "origin" );
-		GUI.Box( new Rect( Screen.width - 10, (1 - cam.rect.y) * Screen.height - 10, 10, 10), "end" );
-
-
 		GUILayout.BeginArea( new Rect(	cam.rect.x * Screen.width,
 										(1 - cam.rect.height - cam.rect.y) * Screen.height,
 										cam.rect.width * Screen.width,
@@ -18,19 +14,7 @@ public class PlayerGUI : MonoBehaviour {
 
 		// Make a background box
 		GUILayout.Box( "GUI" );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
-		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
+
 		GUILayout.Label( "Speed: " + (int)rigidbody.velocity.magnitude );
 
 		GUILayout.EndArea();
