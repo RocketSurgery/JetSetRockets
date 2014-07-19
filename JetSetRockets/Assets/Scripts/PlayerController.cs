58332b9be3980907384a91f328957a1c5e0e44c4
David LeGare <excaliburHisSheath@gmail.com>
2014-07-18 21:28:44 -0700

    Working 3rd Person Camera

--- JetSetRockets/Assets/Scripts/PlayerController.cs
+++ JetSetRockets/Assets/Scripts/PlayerController.cs
@@ -3,33 +3,20 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	private Transform playerCamera;
-
 	public float baseMoveSpeed = 10f;
 
 	public float orbitSpeed = 500f;
 	public float orbitDistance = 10f;
 
 	// Use this for initialization
-	void Start ()
-	{
-		playerCamera = Camera.main.transform;
-	}
-	
+	void Start () { }
+
 	// Update is called once per frame
 	void Update ()
 	{
 		float yRot = Input.GetAxis("Mouse X");
 
 		// rotate Player Controller
-		playerCamera.RotateAround( transform.position, transform.up, yRot * Time.deltaTime * orbitSpeed );
-		playerCamera.LookAt( transform );
-
-		// make player face in correct direction
-		Vector3 offset = ( playerCamera.position - transform.position ).normalized;
-		transform.LookAt( transform.position - offset );
-
-		// reset camera offset from player
-		playerCamera.position = transform.position + offset * orbitDistance;
+		transform.RotateAround( transform.position, transform.up, yRot * Time.deltaTime * orbitSpeed );
 	}
 }
