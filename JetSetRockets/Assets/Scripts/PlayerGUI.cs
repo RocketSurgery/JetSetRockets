2234473e53cc6539cc6e94bc55d3143da51ca19b
David LeGare <excaliburHisSheath@gmail.com>
2014-07-19 20:42:15 -0500

    Slightly Smoother Speed Bar The speed bar was super jaggy and would jump all over the place. I smoothed its motion a little bit by averaging the target length (determined by the speed) with the previous length.

--- JetSetRockets/Assets/Scripts/PlayerGUI.cs
+++ JetSetRockets/Assets/Scripts/PlayerGUI.cs
@@ -6,16 +6,21 @@ public class PlayerGUI : MonoBehaviour {
 	[HideInInspector] public Camera cam;
 	[HideInInspector] public int score = 0;
 	[HideInInspector] public PlayerPhysics physics;
+	float barWidth = 0f;
 
 	void OnGUI()
 	{
+		// update bar width by averaging previous width and target width
+		float targetWidth = rigidbody.velocity.magnitude / physics.MaxSpeed * Screen.width;
+		barWidth = ( barWidth + targetWidth ) / 2;
+
 		GUILayout.BeginArea( new Rect(	cam.rect.x * Screen.width,
 										(1 - cam.rect.height - cam.rect.y) * Screen.height,
 										cam.rect.width * Screen.width,
 										(1 - cam.rect.y) * Screen.height ) );
 
 		// Make a background box
-		GUILayout.BeginArea( new Rect( 0, 0, rigidbody.velocity.magnitude / physics.MaxSpeed * Screen.width, 20) );
+		GUILayout.BeginArea( new Rect( 10, 10, barWidth, 20) );
 		GUILayout.Box( "" );
 		GUILayout.EndArea();
 
